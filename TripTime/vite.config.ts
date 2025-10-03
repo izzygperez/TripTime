3c3cd4f91936958cf7b6276705a9caa011ccf7df
Riana-S <rianasantos808@gmail.com>
2025-10-02 16:29:22 -1000

    Added Ssl

--- TripTime/vite.config.ts
+++ TripTime/vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import webSpatial from "@webspatial/vite-plugin";
 import { createHtmlPlugin } from "vite-plugin-html";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    webSpatial(),
+    react({
+      jsxImportSource: '@webspatial/react-sdk'
+    }),
+    basicSsl(),
+    webSpatial({
+      outputDir: ""
+    }),
     createHtmlPlugin({
       inject: {
         data: {
